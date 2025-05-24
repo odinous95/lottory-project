e89ad71be9cfeb5c5c774789e3badb36472292d3
Odin Thif <mjd.thif@icloud.com>
2025-05-24 21:00:24 +0200

    basic project set up for Lotto contract

---
+++ src/Lotto.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+/// @title Lotto Contract
+/// @author Odin
+/// @notice This contract implements a lottery system where participants can enter for a chance to win a prize.
+/// @dev Provides functions to enter the lottery, pick a winner, and manage the lottery state.
+
+contract Lotto {
+    uint256 public immutable i_entryFee;
+
+    constructor(uint256 entryFee) {
+        i_entryFee = entryFee;
+    }
+
+    function enterLottery() public payable {}
+
+    function pickWinner() public returns (address) {}
+
+    /*
+    Getter Functionsa 
+    **/
+
+    function getEntryFee() public view returns (uint256) {
+        return i_entryFee;
+    }
+}
